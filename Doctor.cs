f4fd59aef89683259a77c03ff46a5b2f0e74b7a3
Wojciech Dąbrowski <wojt648@gmail.com>
2025-01-16 20:26:51 +0100

    Implemented Login Logic, as well as Doctor class

---
+++ Doctor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Admin_System
+{
+    public class Doctor : Employee
+    {
+        public enum Specialization
+        {
+            Cardiologist,
+            Urologist,
+            Neurologist,
+            Laryngologist
+        }
+
+        public Specialization Specialty { get; set; }
+        private string _pwz;
+        private readonly Dictionary<int, List<DateTime>> _onCallSchedule = new();
+
+        public string PWZ
+        {
+            get => _pwz;
+            set
+            {
+                if (value.Length == 7 && value.All(char.IsDigit))
+                {
+                    _pwz = value;
+                }
+                else
+                {
+                    throw new ArgumentException("PWZ must be 7 digits long.");
+                }
+            }
+        }
+
+        public Doctor(string name, string surname, int pesel, string username, string password, Specialization specialty, string pwz, Role role)
+            : base(name, surname, pesel, username, password, role)
+        {
+            Specialty = specialty;
+            PWZ = pwz;
+        }
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Admin_System
+{
+    public abstract class Employee
+    {
+        public enum Role
+        {
+            Doctor,
+            Nurse,
+            Administrator
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        private int _pesel;
+        public int PESEL
+        {
+            get { return _pesel; }
+            private set
+            {
+                if (value.ToString().Length == 11)
+                {
+                    _pesel = value;
+                }
+                else
+                {
+                    throw new ArgumentException("PESEL must be an 11-digit number.");
+                }
+            }
+        }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Role UserRole { get; set; }
+
+        protected static readonly Dictionary<DateTime, Employee> OnCallSchedule = new();
+
+        public Employee(string name, string surname, int pesel, string username, string password, Role userRole)
+        {
+            Name = name;
+            Surname = surname;
+            PESEL = pesel;
+            Username = username;
+            Password = password;
+            UserRole = userRole;
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + Surname + "(" + Username + ")";
+        }
+
+        public bool Login(string username, string password)
+        {
+            return this.Password == password && this.Username == username;
+        }
+
+    }
+}
